21d01df670f79f3b72487d57b4a0e98959a89886
yurison <yu_ri_son@naver.com>
2023-09-04 14:00:56 +0900

    Init

---
+++ src/main/java/threekitties/tonghaeyo/TonghaeyoApplication.java
@@ -0,0 +1,13 @@
+package threekitties.tonghaeyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TonghaeyoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TonghaeyoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/threekitties/tonghaeyo/TonghaeyoApplicationTests.java
@@ -0,0 +1,13 @@
+package threekitties.tonghaeyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TonghaeyoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

f62ab4228b696ebfc08b72e5edc511e65c919be7
yurison <yu_ri_son@naver.com>
2023-09-04 16:30:36 +0900

    초기화 데이터 수정

--- src/main/java/threekitties/tonghaeyo/base/DataLoader.java
+++ src/main/java/threekitties/tonghaeyo/base/DataLoader.java
@@ -3,10 +3,9 @@ package threekitties.tonghaeyo.base;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-import threekitties.tonghaeyo.domain.Authority;
-import threekitties.tonghaeyo.domain.Group;
+import threekitties.tonghaeyo.domain.Organization;
 import threekitties.tonghaeyo.domain.Member;
-import threekitties.tonghaeyo.repository.GroupRepository;
+import threekitties.tonghaeyo.repository.OrganizationRepository;
 import threekitties.tonghaeyo.repository.MemberRepository;
 
 import static threekitties.tonghaeyo.domain.Authority.*;
@@ -15,21 +14,21 @@ import static threekitties.tonghaeyo.domain.Authority.*;
 @Component
 public class DataLoader implements CommandLineRunner {
 
+    private final OrganizationRepository organizationRepository;
     private final MemberRepository memberRepository;
-    private final GroupRepository groupRepository;
 
     @Override
     public void run(String... args) throws Exception {
-        Group group1 = groupRepository.save(Group.builder().name("삼냥이 유치원").build());
-        Group group2 = groupRepository.save(Group.builder().name("삼냥이 고등학교").build());
+        Organization organization1 = organizationRepository.save(Organization.builder().name("삼냥이 유치원").build());
+        Organization organization2 = organizationRepository.save(Organization.builder().name("삼냥이 고등학교").build());
 
-        memberRepository.save(Member.builder().name("manager1").group(group1).authority(MANAGER).build());
-        memberRepository.save(Member.builder().name("driver1").group(group1).authority(DRIVER).build());
-        memberRepository.save(Member.builder().name("yuri").group(group1).authority(USER).build());
+        memberRepository.save(Member.builder().name("manager1").organization(organization1).authority(MANAGER).build());
+        memberRepository.save(Member.builder().name("driver1").organization(organization1).authority(DRIVER).build());
+        memberRepository.save(Member.builder().name("yuri").organization(organization1).authority(USER).build());
 
-        memberRepository.save(Member.builder().name("manager2").group(group2).authority(MANAGER).build());
-        memberRepository.save(Member.builder().name("driver2").group(group2).authority(DRIVER).build());
-        memberRepository.save(Member.builder().name("hyein").group(group2).authority(USER).build());
+        memberRepository.save(Member.builder().name("manager2").organization(organization2).authority(MANAGER).build());
+        memberRepository.save(Member.builder().name("driver2").organization(organization2).authority(DRIVER).build());
+        memberRepository.save(Member.builder().name("hyein").organization(organization2).authority(USER).build());
     }
 
 }

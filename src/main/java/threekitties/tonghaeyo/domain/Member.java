2017081f81548504ea853069d589733e9b8c9013
miknieyh <tong_hye@naver.com>
2023-09-04 14:45:18 +0900

    Data 어노테이션 삭제

--- src/main/java/threekitties/tonghaeyo/domain/Member.java
+++ src/main/java/threekitties/tonghaeyo/domain/Member.java
@@ -1,19 +1,17 @@
 package threekitties.tonghaeyo.domain;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
-import threekitties.tonghaeyo.domain.Authority;
 @Entity
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
 @Table(name = "member")
 public class Member {
     @Id

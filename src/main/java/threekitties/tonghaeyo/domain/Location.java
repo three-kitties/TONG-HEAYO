e069df50eefaba0211282f316193a556a33fb7fd
yurison <yu_ri_son@naver.com>
2023-09-05 12:34:26 +0900

    Route, Location 엔티티 생성

---
+++ src/main/java/threekitties/tonghaeyo/domain/Location.java
@@ -0,0 +1,27 @@
+package threekitties.tonghaeyo.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Location {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Long lat;
+    private Long lng;
+
+    public Location(Long lat, Long lng) {
+        this.lat = lat;
+        this.lng = lng;
+    }
+
+}

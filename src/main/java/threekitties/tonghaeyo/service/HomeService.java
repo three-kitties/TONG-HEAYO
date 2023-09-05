2dd63093a272215580d860afc7b08992aa87e3a2
miknieyh <tong_hye@naver.com>
2023-09-05 12:17:40 +0900

    login 세션저장

--- src/main/java/threekitties/tonghaeyo/service/HomeService.java
+++ src/main/java/threekitties/tonghaeyo/service/HomeService.java
@@ -1,14 +1,19 @@
 package threekitties.tonghaeyo.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import threekitties.tonghaeyo.domain.Authority;
+import threekitties.tonghaeyo.domain.Member;
+import threekitties.tonghaeyo.repository.MemberRepository;
 
 @RequiredArgsConstructor
 @Service
 public class HomeService {
-
-    public String login(String id){
-
+    @Autowired
+    private MemberRepository repository;
+    public Member login(String name){
+        return repository.findByName(name).get();
     }
 
 }
